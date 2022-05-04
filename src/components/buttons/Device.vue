ecdcfb37ae75bbe160dfd978b53d24fc159bed47
Martin Zahnd <mzahnd@itba.edu.ar>
2022-05-04 16:02:18 -0300

    Add DeviceButton template
    Alongisde the DeviceButton template, this commits has an icon (black
    and white and color versions) to try it.

---
+++ src/components/buttons/Device.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card-actions>
+    <v-btn
+        color="secondary"
+        height="110"
+        width="110"
+        class="d-flex flex-column btnDevice"
+        style="color:black"
+        elevation="3"
+        raised
+        @click="toggleImage()"
+    >
+      <v-img
+          max-height="65"
+          max-width="65"
+          :src="getImage()"
+          class="ma-1 pa-0"
+      ></v-img>
+      <v-card-text
+          class="ma-1 pa-1"
+      >
+        {{ this.title }}
+      </v-card-text>
+    </v-btn>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: "BtnDevice",
+  props: {
+    'title': String,
+    'imgOff': String,
+    'imgOn': String,
+  },
+  data() {
+    return {
+      isActivated: false
+    }
+  },
+  methods: {
+    getImage() {
+      if (this.isActivated) {
+        return require(`@/assets/${this.imgOn}`)
+      } else {
+        return require(`@/assets/${this.imgOff}`);
+      }
+    },
+    toggleImage() {
+      this.isActivated = !this.isActivated;
+    }
+  }
+}
+</script>
+
+<style>
+.btnDevice > span {
+  flex-direction: column !important;
+}
+</style>
