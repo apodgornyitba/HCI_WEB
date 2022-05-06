e3f56349bb4d2c619fe90013b63159b3ada8b576
Martin Zahnd <mzahnd@itba.edu.ar>
2022-05-06 00:20:27 -0300

    Create horizontal container for homes and devices
    This component allows including multiple elements inside the container
    as well as a title for it.

--- src/components/cardHomes.vue
+++ src/components/cardHomes.vue
@@ -1,22 +1,46 @@
 <template>
-  <v-card
+<!--  <v-card
       outlined
       style="margin-left: 50px; margin-top: 50px; margin-bottom: 150px"
       max-width="900"
   >
-    <BtnDevice
+    <btn-device
       title="Hogar 1"
-      img-off="icons/home-bw-512px.png"
-      img-on="icons/home-color-512px.png"
+      image-off="icons/home-bw-512px.png"
+      image-on="icons/home-color-512px.png"
     />
-  </v-card>
+  </v-card>-->
+  <container-horizontal
+      title="Hogares"
+  >
+    <btn-device
+        title="Hogar 1"
+        image-off="icons/home-bw-512px.png"
+        image-on="icons/home-color-512px.png"
+        class="my-0 mx-1 pa-0"
+    />
+    <btn-device
+        title="Hogar 2"
+        image-off="icons/home-bw-512px.png"
+        image-on="icons/home-color-512px.png"
+        class="my-0 mx-1 pa-0"
+    />
+    <btn-device
+        title="Hogar 3"
+        image-off="icons/home-bw-512px.png"
+        image-on="icons/home-color-512px.png"
+        class="my-0 mx-1 pa-0"
+    />
+  </container-horizontal>
+
 </template>
 
 <script>
 import BtnDevice from "@/components/buttons/Device";
+import ContainerHorizontal from "@/components/containers/ContainerHorizontal";
 export default {
   name: "cardHomes",
-  components: {BtnDevice}
+  components: {ContainerHorizontal, BtnDevice}
 }
 </script>
 
---
+++ src/components/containers/ContainerHorizontal.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container
+      class="ma-0 pa-0"
+  >
+    <v-row
+        class="my-6 mx-4 pa-1"
+        no-gutters
+    >
+      <v-card-text
+          class="ma-2 pa-0 text-h2"
+      >
+        {{ this.title }}
+      </v-card-text>
+      <v-container
+          class="ma-0 pa-0"
+      >
+        <v-card
+            class="ma-0 my-1 py-4 px-4"
+            outlined
+        >
+          <v-row
+              class="ma-0 pa-0 justify-space-around"
+          >
+            <slot></slot>
+          </v-row>
+        </v-card>
+      </v-container>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ContainerHorizontal",
+  props: {
+    'title': String,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
