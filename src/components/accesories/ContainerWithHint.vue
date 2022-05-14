56d7a88d1a4d972c93907e3a55c66ecce8a31981
Martin Zahnd <mzahnd@itba.edu.ar>
2022-05-14 04:10:40 -0300

    Add container with optional hint at the bottom This is a wrapper intended to allow us to add a hint message at the bottom of anything we so desire.

---
+++ src/components/accesories/ContainerWithHint.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row
+      no-gutters
+      :class="classes.row"
+  >
+    <slot/>
+    <v-card-text
+        v-if="this.hint"
+        :class="classes.text"
+        style="width: fit-content;"
+    >
+      {{ this.hint }}
+    </v-card-text>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ContainerWithHint",
+  props: {
+    hint: String,
+  },
+  computed: {
+    classes: () => ({
+      row: {
+        'justify-center': true,
+        'align-center': true,
+      },
+      text: {
+        'ma-0': true,
+        'pa-0': true,
+        'my-3': true,
+        'text-center': true,
+        'txt--text': true,
+        'text-subtitle-2': true,
+        'text--darken-1': true,
+      }
+    }),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
