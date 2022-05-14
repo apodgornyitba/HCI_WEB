8c42572a95eda1ba30c1821f546a2414a9f300b1
Martin Zahnd <mzahnd@itba.edu.ar>
2022-05-14 04:12:15 -0300

    Add consistent text field for website Wrapper around v-text-field using ContainerWithHint to keep consistentency between components.

---
+++ src/components/accesories/txt-field.vue
@@ -0,0 +1,36 @@
+<template>
+  <container-with-hint
+      :hint="this.hint"
+  >
+      <v-text-field
+          v-bind="$attrs"
+          :label="this.label"
+          outlined
+          clearable
+          hide-details
+      />
+  </container-with-hint>
+</template>
+
+<script>
+import ContainerWithHint from "@/components/accesories/ContainerWithHint";
+
+export default {
+  name: "txt-field",
+  components: {ContainerWithHint},
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    hint: {
+      type: String,
+      required: true
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
