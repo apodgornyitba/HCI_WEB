2c9000c87524ae35fcc48a9776b4f9e20c6cee4d
Martin Zahnd <mzahnd@itba.edu.ar>
2022-05-19 17:30:30 -0300

    Header update :D

--- src/components/accesories/Header.vue
+++ src/components/accesories/Header.vue
@@ -3,38 +3,80 @@
       app
       color="quaternary base"
       flat
-      class="rounded-lg rounded-t-0"
+      class="rounded-lg rounded-t-0 justify-end"
   >
-    <div class="d-flex align-center">
-
-      <router-link :to="(this.altHomePath && this.user.id) ? this.altHomePath : '/'">
-        <v-img
-            :src="require('@/assets/logo-name.png')"
-        />
-      </router-link>
-    </div>
-
-    <v-col
-        offset-sm="6"/>
-    <v-btn v-if="user.id"
-           color="black"
-           to="/mainScreen"
-           text
+    <v-row
+        class="align-center justify-space-between"
     >
-      MI HOGAR
-    </v-btn>
+      <v-col>
+        <div class="my-auto mx-5 pa-0">
+          <router-link :to="(this.altHomePath && this.user.id) ? this.altHomePath : '/'">
+            <v-img
+                :src="require('@/assets/logo-name.png')"
+            />
+          </router-link>
+        </div>
+      </v-col>
 
-    <div v-if="user.id">
-      <strong style="color:black"> {{ user.name }} </strong>
-      <v-btn
-          color="black"
-          icon
-          to="/"
-          @click="logOutHandler"
-      >
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
-    </div>
+      <v-spacer class="hidden-sm-and-down"/>
+
+      <v-col class="ma-0 my-auto mx-10 pa-0 align-center justify-end">
+        <v-row
+          class="align-center justify-end"
+        >
+          <v-btn v-if="user.id"
+                 color="black"
+                 to="/mainScreen"
+                 text
+          >
+            MI HOGAR
+          </v-btn>
+
+          <div v-if="user.id">
+            <strong
+                class="ma-0 my-auto mx-5 pa-0"
+                style="color:black"
+            >
+              {{ user.name }}
+            </strong>
+            <v-btn
+                color="black"
+                icon
+                to="/"
+                @click="logOutHandler"
+            >
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </div>
+        </v-row>
+      </v-col>
+    </v-row>
+    <!--
+        <v-col>
+          <v-btn v-if="user.id"
+                 color="black"
+                 to="/mainScreen"
+                 text
+          >
+            MI HOGAR
+          </v-btn>
+        </v-col>
+
+        <v-col>
+          <div v-if="user.id">
+            <strong style="color:black"> {{ user.name }} </strong>
+            <v-btn
+                color="black"
+                icon
+                to="/"
+                @click="logOutHandler"
+            >
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+    -->
+
   </v-app-bar>
 </template>
 
